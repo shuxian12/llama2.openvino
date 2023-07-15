d566a9ddf93df5b18d0a91b45f6030ee98231237
ethan <ethan.yang@intel.com>
2023-07-15 16:54:41 +0000

    formatting

--- export.py
+++ export.py
@@ -4,10 +4,19 @@ from transformers import LlamaForCausalLM
 import argparse
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
-parser.add_argument('-m', '--model_path',  required=True, type=str,
+parser.add_argument('-h',
+                    '--help',
+                    action='help',
+                    help='Show this help message and exit.')
+parser.add_argument('-m',
+                    '--model_path',
+                    required=True,
+                    type=str,
                     help='Required. orignal model path')
-parser.add_argument('-o', '--onnx_path',  required=True, type=str,
+parser.add_argument('-o',
+                    '--onnx_path',
+                    required=True,
+                    type=str,
                     help='Required. onnx model path')
 args = parser.parse_args()
 
@@ -18,7 +27,17 @@ inputs = {
 }
 model.eval()
 with torch.no_grad():
-    torch_onnx_export(model, inputs, args.onnx_path,input_names=list(inputs), dynamic_axes={
-                          list(inputs)[0]: {0: 'batch_size', 1: 'sequence_length'},
-                          list(inputs)[1]: {0: 'batch_size', 1: 'sequence_length'}}
-                      )
+    torch_onnx_export(model,
+                      inputs,
+                      args.onnx_path,
+                      input_names=list(inputs),
+                      dynamic_axes={
+                          list(inputs)[0]: {
+                              0: 'batch_size',
+                              1: 'sequence_length'
+                          },
+                          list(inputs)[1]: {
+                              0: 'batch_size',
+                              1: 'sequence_length'
+                          }
+                      })
